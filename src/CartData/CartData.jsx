3f362b7ae35c9b76fae5d97767cb1546cfba8beb
userFbi <pawartushar2024.katargam@gmail.com>
2026-01-22 09:49:53 +0530

    file modified

--- src/CartData/CartData.jsx
+++ src/CartData/CartData.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useFormik } from 'formik';
 
 const CartData = () => {
-    return (
-        <div>
-            <h1>helo</h1>
-        </div>
-    )
-}
 
+    const [list, setList] = useState([])
+
+    const formik = useFormik({
+        initialValues: {
+            firstName: '',
+            lastName: '',
+            email: '',
+        },
+        onSubmit: (values, { resetForm }) => {
+            setList(JSON.stringify(values, null, 2));
+            resetForm()
+        },
+    });
+    return (
+        <form onSubmit={formik.handleSubmit}>
+            <br /><br />
+            <label htmlFor="firstName">First Name</label>
+            <input
+                required
+                id="firstName"
+                name="firstName"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.firstName}
+            />
+            <br /><br />
+            <label htmlFor="lastName">Last Name</label>
+            <input
+                required
+                id="lastName"
+                name="lastName"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.lastName}
+            />
+            <br /><br />
+            <label htmlFor="email">Email Address</label>
+            <input
+                required
+                id="email"
+                name="email"
+                type="email"
+                onChange={formik.handleChange}
+                value={formik.values.email}
+            /><br /><br />
+
+            <button type="submit">Submit</button>
+        </form>
+
+
+        // {
+        //     list.map((i,index)=> {
+
+        //     })
+        // }
+
+    );
+};
 export default CartData
